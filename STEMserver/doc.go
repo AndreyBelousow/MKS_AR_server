44f070e5e79587342ba40bb8bdc8fbb7203e54f7
Andrey Belousow <groundhog448@gmail.com>
2017-04-19 16:38:24 +0400

    Первая версия

---
+++ STEMserver/doc.go
@@ -0,0 +1,6 @@
+// STEMserver project doc.go
+
+/*
+STEMserver document
+*/
+package main
---
+++ STEMserver/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+
+	"os"
+
+	"github.com/go-martini/martini"
+
+	"io/ioutil"
+)
+
+func main() {
+
+	fmt.Println("SERVER IS RUNNING....")
+
+	m := martini.Classic()
+
+	m.Get("/get", func(res http.ResponseWriter, r *http.Request) {
+
+		//Директория с файлом
+		//Для продакшн сервера
+		//path := os.Getenv("HOME") + "/STEMserver"
+		//Для теста
+		path := "C:/gopath/src/STEMserver/schedule.xml"
+
+		reqFile, err := os.Open(path)
+		if err != nil {
+			res.WriteHeader(1488)
+			res.Write([]byte(err.Error()))
+		} else {
+			//Считываем файл в массив байт
+			fi, err := reqFile.Stat()
+			if err != nil {
+				res.WriteHeader(1488)
+				res.Write([]byte(err.Error()))
+			} else {
+				size := fi.Size()
+				var bytes = make([]byte, size)
+				reqFile.Read(bytes)
+
+				//Отправляем
+				res.WriteHeader(200)
+				res.Write(bytes)
+			}
+			defer reqFile.Close()
+		}
+	})
+
+	m.Post("/upload", func(res http.ResponseWriter, r *http.Request) {
+		file, err := os.Create("schedule.xml")
+		if err != nil {
+			res.WriteHeader(1488)
+			res.Write([]byte(err.Error()))
+			defer file.Close()
+		} else {
+			defer file.Close()
+			tmp, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				res.WriteHeader(1488)
+				res.Write([]byte(err.Error()))
+			} else {
+				file.Write(tmp)
+				defer file.Close()
+				res.WriteHeader(200)
+			}
+		}
+	})
+
+	m.Run()
+}
